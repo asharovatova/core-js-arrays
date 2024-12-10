4600cfb369c4fcd0972ce06afbc90f869af50388
asharovatova <co.rin@mail.ru>
2024-12-10 14:48:21 +0300

    feat: implement getElementByIndices

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -393,8 +393,12 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  if (indices.length === 1) return arr[indices[0]];
+
+  const elemArr = arr[indices[0]];
+  const newIndeces = indices.slice(1);
+  return getElementByIndices(elemArr, newIndeces);
 }
 
 /**
